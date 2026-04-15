2fcc229529aa35c7a52a1ea7b55f3e4e5aa6b4da
ven-gnz <kangas.otso@gmail.com>
2026-04-15 13:40:27 +0300

    Auto generated expr quickeroo

--- src/main/java/pulp/Expr.java
+++ src/main/java/pulp/Expr.java
@@ -1,22 +1,37 @@
 package pulp;
+import java.util.List;
 
-abstract class Expr {
-
-    static class Binary extends Expr
-    {
-
-        final Expr left;
-        final Expr right;
-        final Token operator;
-
-        Binary(Expr left,Token operator,Expr right)
-        {
-            this.left = left;
-            this.operator = operator;
-            this.right = right;
-        }
-
+abstract class Expr{
+ static class Literal extends Expr {
+    Literal(Object value) {
+    this.value = value;
     }
 
+    final Object value;
+  }
+ static class Identifier extends Expr {
+    Identifier(String name) {
+    this.name = name;
+    }
 
+    final String name;
+  }
+ static class Assign extends Expr {
+    Assign(String name, Expr value) {
+    this.name = name;
+    this.value = value;
+    }
+
+    final String name;
+    final  Expr value;
+  }
+ static class Add extends Expr {
+    Add(Expr left, Expr right) {
+    this.left = left;
+    this.right = right;
+    }
+
+    final Expr left;
+    final  Expr right;
+  }
 }
