6ecc57fa30ddc2b7578ea742981f09c45ebfbe22
ven-gnz <kangas.otso@gmail.com>
2026-04-23 15:43:35 +0300

    Update block statement + gen

--- src/main/java/pulp/Stmt.java
+++ src/main/java/pulp/Stmt.java
@@ -5,10 +5,23 @@ import pulp.Expr;
 
 abstract class Stmt{
     interface Visitor<R> {
+    R visitBlockStmt(Block stmt);
     R visitExpressionStmt(Expression stmt);
     R visitPrintStmt(Print stmt);
     R visitVarStmt(Var stmt);
  }
+ static class Block extends Stmt {
+    Block(List<Stmt> statements) {
+    this.statements = statements;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+    return visitor.visitBlockStmt(this);
+    }
+
+    final List<Stmt> statements;
+  }
  static class Expression extends Stmt {
     Expression(Expr expression) {
     this.expression = expression;
